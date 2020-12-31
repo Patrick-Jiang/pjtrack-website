7dd47fc5a3fd86e650927e3681bccddb9e8f4ab2
patrickjiang <patrick.jiang86@gmail.com>
2020-12-31 00:50:29 -0500

    deploy

---
+++ precache-manifest.24d40c6094767d61aa3a9a9fc84bd244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3cfbb5496a92e5e482",
+    "url": "/pjtrack-website/css/app.30fe3f92.css"
+  },
+  {
+    "revision": "6354dca4d0ce82295aad3e8506d494fb",
+    "url": "/pjtrack-website/index.html"
+  },
+  {
+    "revision": "4e3cfbb5496a92e5e482",
+    "url": "/pjtrack-website/js/app.f73338ee.js"
+  },
+  {
+    "revision": "b70656a33c83cef9a68f",
+    "url": "/pjtrack-website/js/chunk-vendors.cfd73902.js"
+  },
+  {
+    "revision": "faae1367978523fb9fe3d9346ccddf5f",
+    "url": "/pjtrack-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pjtrack-website/robots.txt"
+  }
+]);
